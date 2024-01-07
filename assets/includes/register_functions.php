b11c5d932b492c262efb24592193df1c44f66843
Kordit <kontakt@kordit.pl>
2024-01-07 23:43:52 +0100

    poprawka

--- assets/includes/register_functions.php
+++ assets/includes/register_functions.php
@@ -262,8 +262,8 @@ function et_get_fields_acf($single_acf, $depth = 1, $classname = '')
 		$type_field .= '    while ($moj_cpt_query->have_posts()) : $moj_cpt_query->the_post();' . PHP_EOL;
 		$type_field .= ' $fields = get_fields(get_the_ID());' . PHP_EOL;
 		$type_field .= '  if ($fields) {' . PHP_EOL;
-		$type_field .= '     acf_setup_meta($fields, get_the_ID(), true);' . PHP_EOL;
-		$type_field .= '  }' . PHP_EOL;
+		$type_field .= '     acf_setup_meta($fields, get_the_ID(), true); ' . PHP_EOL;
+		$type_field .= '  } ?>' . PHP_EOL;
 		$type_field .= '        <?php the_post_thumbnail(); ?>' . PHP_EOL;
 		$type_field .= '        <?php the_title(); ?>' . PHP_EOL;
 		$allFields = [];
