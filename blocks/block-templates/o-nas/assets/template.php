f3594aa215f86b9e55ebb38c59ac078f9b57b79d
Kordit <kontakt@kordit.pl>
2024-01-31 22:26:04 +0100

    usuniecie domyslnych blokow

--- blocks/block-templates/o-nas/assets/template.php
+++
@@ -1,7 +0,0 @@
-<div class="heading-2">
-<?php the_field('tytul'); ?>
-</div>
-<div class="description smaller">
-<?php the_field('description'); ?>
-</div>
-<?php et_image('image', 'full', false, 'o-nas--image'); ?>
